1113a7ac0fcfc377ab0139718d2ffe9f19e4d04c
Michael Calmer <mc@suse.de>
2015-03-26 10:14:29 +0100

    reformat template and enable auto trimming

--- utils/skus-scanner.py
+++ utils/skus-scanner.py
@@ -134,7 +134,7 @@ if printGroups:
     od = collections.OrderedDict(sorted(Groups.items(), key=lambda t: t[0]))
 
     templateLoader = jinja2.FileSystemLoader( searchpath="./" )
-    templateEnv = jinja2.Environment( loader=templateLoader )
+    templateEnv = jinja2.Environment( loader=templateLoader, trim_blocks=True, lstrip_blocks=True )
     template = templateEnv.get_template("./PartNumbers.drl.jinja")
 
     template.stream(obj=Obj).dump("./PartNumbers.drl")

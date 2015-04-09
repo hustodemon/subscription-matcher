c62c0d84111d023f00755af7119a1b1253f79132
Michael Calmer <mc@suse.de>
2015-04-09 17:07:24 +0200

    create rules for new partnumbers as well

--- utils/skus-scanner.py
+++ utils/skus-scanner.py
@@ -9,9 +9,10 @@ import collections
 import jinja2
 
 printGroups = True
-printUnknown = False
+printUnknown = True
 
 Groups = {}
+Partnumbers = {}
 Unknown = []
 Obj = []
 
@@ -21,11 +22,15 @@ def parseRow(row):
     cpu = None      # I = Instance, <num>cpus, <num>ifls
     virt = "UV"     # UV = unlimited Virtualization, PHY = physical, 2=2 = 2 Sockets or 2 Virtual, INH = inherit
     stack = "N"     # N = non-stackable, S = Stackable
-    support = "BS"  # BS = Basic, ST = Standard, PR = Priority, INH = inherit
+    support = "BS"  # BS = Basic, ST = Standard, PR = Priority, INH = inherit, L3PR = vendor priority, L3ST = vendor standard
     quantityFactor = None
 
     desc = row['Long Item Description:'].lower()
     pnum = row['Item Number:']
+
+    if pnum in Partnumbers:
+        # duplicate
+        return
 
     # ignore bundels. Needs special handling
     if pnum in ('051-004968','051-004972','051-004969',
@@ -139,6 +144,114 @@ def parseRow(row):
                "desc": desc,
                "quantityFactor": quantityFactor}
 
+    Partnumbers[pnum] = obj
+    if not Groups.has_key(group):
+        Groups[group] = []
+    Groups[group].append((pnum, desc))
+    Obj.append(obj)
+
+def parseRowNew(row):
+    cpu = None      # I = Instance, <num>cpus, <num>ifls
+    virt = "UV"     # UV = unlimited Virtualization, PHY = physical, 2=2 = 2 Sockets or 2 Virtual, INH = inherit
+    stack = "S"     # N = non-stackable, S = Stackable
+    support = "BS"  # BS = Basic, ST = Standard, PR = Priority, INH = inherit
+    quantityFactor = None
+
+    desc = row['Product Description'].lower()
+    pnum = row['Part Number']
+
+    if pnum in Partnumbers:
+        # duplicate
+        return
+
+    # ignore bundels. Needs special handling
+    #if pnum in (''):
+    #    Unknown.append(row)
+    #    return
+    if "1-2 sockets or 1-2 virtual" in desc:
+        cpu = "2"
+        virt = "2=2"
+    elif recpu.search(desc):
+        num = recpu.search(desc).groups()[0]
+        cpu = num
+        #print "CPUS: %s - %s" % (cpu, desc)
+    elif " ifl" in desc:
+        cpu = "1"
+    elif (" hosted " in desc or
+        " 1-instance" in desc or
+        " 1 instance" in desc or
+        " 1-device" in desc or
+        " 1 physical server" in desc or
+        " 1 scom instance" in desc or
+        " additional instance" in desc):
+        cpu = "I"
+    elif " 1-2 instances" in desc:
+        cpu = "I"
+        quantityFactor = 2
+
+    if " unlimited virtual machines" in desc:
+        virt = "UV"
+    elif "inherited virtualization" in desc:
+        virt = "INH"
+
+    if "l3-priority" in desc:
+        support = "L3PR"
+    elif "priority" in desc:
+        support = "PR"
+    elif "l3-standard" in desc:
+        support = "L3ST"
+    elif "standard" in desc:
+        support = "ST"
+    elif "inherited subscription" in desc:
+        support = "INH"
+
+    if "long term service pack" in desc:
+        cpu = "I"
+        virt = "INH"
+        stack = "N"
+        support = "INH"
+        if "unlimited servers" in desc:
+            quantityFactor = -1
+        elif "500 servers" in desc:
+            quantityFactor = 500
+        elif "100 servers" in desc:
+            quantityFactor = 100
+        elif "10 ifls" in desc:
+            quantityFactor = 10
+        elif "5 ifls" in desc:
+            quantityFactor = 5
+        elif "unlimited ifls" in desc:
+            quantityFactor = -1
+        else:
+            #print "Unknown quantityFactor for %s" % desc
+            cpu = None
+            return
+
+    if cpu is None:
+        Unknown.append({'Item Number:': pnum,
+                        'Long Item Description:': desc})
+        return
+
+    if cpu == "I":
+        group = "I-%s-%s-%s" % (virt, stack, support)
+        obj = {"cpu": None,
+               "virt": virt,
+               "stack": stack,
+               "supp": support,
+               "pnum": pnum,
+               "desc": desc,
+               "quantityFactor": quantityFactor}
+    else:
+        group = "%scpus-%s-%s-%s" % (cpu, virt, stack, support)
+        obj = {"cpu": cpu,
+               "virt": virt,
+               "stack": stack,
+               "supp": support,
+               "pnum": pnum,
+               "desc": desc,
+               "quantityFactor": quantityFactor}
+
+    Partnumbers[pnum] = obj
     if not Groups.has_key(group):
         Groups[group] = []
     Groups[group].append((pnum, desc))
@@ -147,7 +260,12 @@ def parseRow(row):
 if not os.path.exists("./legacy_skus.csv"):
     os.system("wget http://w3.suse.de/~mc/SUSEManager/SKUs/legacy_skus.csv")
 
-#with open('./test.csv', 'rb') as csvfile:
+with open('./ihv_isv.csv', 'rb') as csvfile:
+    skusreader = csv.DictReader(csvfile, delimiter=',')
+    for row in skusreader:
+        #print "row: %s" % row
+        parseRowNew(row)
+#with open('./ihv_isv.csv', 'rb') as csvfile:
 with open('./legacy_skus.csv', 'rb') as csvfile:
     skusreader = csv.DictReader(csvfile, delimiter=',', quoting=csv.QUOTE_NONE)
     for row in skusreader:
